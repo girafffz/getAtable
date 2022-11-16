0af41232a923af60e8df38417d18111e897a168c
Steph Luo <steph.luokl@gmail.com>
2022-11-17 03:46:05 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <div className="container h-40 bg-darkBrown mt-10">Footer</div>
+      <div className="container mx-auto px-24 h-40 bg-darkBrown mt-10">
+        Footer
+      </div>
     </footer>
   );
 };
