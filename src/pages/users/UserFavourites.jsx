437aa14ee3c38829de52a7a52a782b8c43d55e7f
Steph Luo <steph.luokl@gmail.com>
2022-11-17 15:44:01 +0800

    Create UserFavourites.jsx

---
+++ src/pages/users/UserFavourites.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect, useContext } from "react";
+// import { useForm } from "react-hook-form";
+import { useNavigate, useParams } from "react-router-dom";
+import LoginContext from "../../context/LoginContext";
+
+const UserFavourites = () => {
+  const { id } = useParams();
+  const loginCtx = useContext(LoginContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (loginCtx.isLogin) {
+      navigate(`/users/${loginCtx.personLogin.id}/favourites`);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!loginCtx.isLogin) {
+      navigate("/users/login");
+      loginCtx.setRestaurantSite(false);
+    }
+  }, []);
+
+  return (
+    <>
+      <div className="container mx-auto text-center mt-10 px-24">
+        <h1 className="text-3xl font-semibold">{`${loginCtx.personLogin.name} ${loginCtx.personLogin.last_name} Favourites`}</h1>
+        <br />
+        <p className="text-xl">Under Construction</p>
+        <br />
+        <p>This page will contains all the user's favourite restaurants</p>
+      </div>
+    </>
+  );
+};
+
+export default UserFavourites;
