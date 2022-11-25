f5921dabd7df0ea0f6378a201e85299cab7fb85f
Steph Luo <steph.luokl@gmail.com>
2022-11-25 18:36:41 +0800

    create and get tables api completed

--- src/pages/restaurants/RestaurantTablesSetting.jsx
+++ src/pages/restaurants/RestaurantTablesSetting.jsx
@@ -4,6 +4,12 @@ import { useNavigate, useParams } from "react-router-dom";
 import LoginContext from "../../context/LoginContext";
 
 const RestaurantTablesSetting = () => {
+  const [tableIsCreated, setTableIsCreated] = useState(false);
+  const [tablesInRestaurant, setTablesInRestaurant] = useState();
+  const [newTableData, setNewTableData] = useState(undefined); // store data for new table to be added to db
+  const [allTablesData, setAllTablesData] = useState(undefined); // store data for all tables retrieved from db
+  const [maxCapacity, setMaxCapacity] = useState(0); // store max capacity data retrieved from db
+
   const { id } = useParams();
   const loginCtx = useContext(LoginContext);
   const navigate = useNavigate();
@@ -23,16 +29,106 @@ const RestaurantTablesSetting = () => {
     }
   }, []);
 
+  // Create a new table
+  useEffect(() => {
+    const createNewTable = async () => {
+      try {
+        const res = await fetch(
+          `http://127.0.0.1:5000/api/restaurants/${id}/capacity`,
+          {
+            method: "PUT",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(newTableData),
+          }
+        );
+        const newTable = await res.json();
+        if (newTable.status === "create successful") {
+          setTableIsCreated(true);
+          reset();
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    createNewTable();
+  }, [newTableData]);
+
+  // Retrieving data from database
+  useEffect(() => {
+    const getAllTablesCreated = async () => {
+      console.log(tableIsCreated);
+      try {
+        const res = await fetch(
+          `http://127.0.0.1:5000/api/restaurants/${id}/capacity`,
+          {
+            method: "GET",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(),
+          }
+        );
+        const allTablesCreated = await res.json();
+        setAllTablesData(allTablesCreated.data.tables);
+        setMaxCapacity(allTablesCreated.data.max_capacity);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getAllTablesCreated();
+  }, [tableIsCreated, id]);
+
+  useEffect(() => {
+    if (allTablesData !== undefined) {
+      renderTablesCreated();
+    }
+  }, [allTablesData, tableIsCreated, id]);
+
+  const renderTablesCreated = () => {
+    const allTablesCreated = allTablesData.map((table) => {
+      return (
+        <div key={`${table.id}`}>
+          <div className=" items-center mb-3 p-6 rounded-lg border border-gray-300 bg-white w-full md:flex">
+            <div className="mt-2 md:flex md:w-2/3 md:space-x-2 md:mt-0">
+              <h5 className="text-xl leading-tight font-medium md:px-3 md:w-1/4 py-1.5">
+                {`Table Number: ${table.table_num}`}
+              </h5>
+              <h5 className="text-xl leading-tight font-medium md:px-3 md:w-1/4 py-1.5">
+                {`Table Capacity: ${table.table_capacity}`}
+              </h5>
+            </div>
+            <div className="space-x-2 mt-2 md:flex md:w-1/3 md:justify-end md:mt-0">
+              <button className="px-3 py-1.5 bg-lightBrown text-white font-medium text-md uppercase rounded shadow-md hover:bg-darkBrown transition duration-150">
+                View
+              </button>
+              <button className="px-3 py-1.5 bg-lightBrown text-white font-medium text-md uppercase rounded shadow-md hover:bg-darkBrown transition duration-150">
+                Edit
+              </button>
+              <button
+                id={`${table.id}`}
+                className="px-3 py-1.5 bg-lightBrown text-white font-medium text-md uppercase rounded shadow-md hover:bg-darkBrown transition duration-150"
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      );
+    });
+    setTablesInRestaurant(allTablesCreated);
+  };
+
   // adding react-hook-forms functionality
   const {
     register,
     handleSubmit,
+    reset,
     setValue,
     formState: { errors },
   } = useForm();
 
   const onSubmit = (data) => {
     console.log(data);
+    setNewTableData(data);
+    setTableIsCreated(false);
   };
 
   const onError = (errors) => console.log(errors);
@@ -48,7 +144,7 @@ const RestaurantTablesSetting = () => {
             <select
               id="table-capacity"
               className="form-select w-full appearance-none px-3 py-1.5 bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out focus:border-darkBrown md:w-48"
-              {...register("table-capacity", {
+              {...register("table_capacity", {
                 required: {
                   value: true,
                   message: "Table capacity is required",
@@ -70,7 +166,7 @@ const RestaurantTablesSetting = () => {
               id="table-num"
               placeholder="Table Number"
               className="form-control w-full px-3 py-1.5 border border-solid border-gray-300 rounded focus:ring-darkBrown md:w-48"
-              {...register("table-num", {
+              {...register("table_num", {
                 required: {
                   value: true,
                   message: "Table number is required",
@@ -91,11 +187,12 @@ const RestaurantTablesSetting = () => {
         <h1 className="text-3xl">Existing Tables</h1>
       </div>
       <div className="container mx-auto mt-5 px-24 md:flex">
-        <h3 className="text-xl">Maximum Capacity: 50</h3>
+        <h3 className="text-xl">{`Maximum Capacity: ${maxCapacity}`}</h3>
       </div>
       <div className="container mx-auto mt-10 px-24">
+        {tablesInRestaurant}
         {/* ---- Table Card ---- */}
-        <div key="id1">
+        {/* <div key="id1">
           <div className=" items-center mb-3 p-6 rounded-lg border border-gray-300 bg-white w-full md:flex">
             <div className="mt-2 md:flex md:w-2/3 md:space-x-2 md:mt-0">
               <h5 className="text-xl leading-tight font-medium md:px-3 md:w-1/4 py-1.5">
@@ -117,8 +214,8 @@ const RestaurantTablesSetting = () => {
               </button>
             </div>
           </div>
-        </div>
-        <div key="id2">
+        </div> */}
+        {/* <div key="id2">
           <div className=" items-center mb-3 p-6 rounded-lg border border-gray-300 bg-white w-full md:flex">
             <div className="mt-2 md:flex md:w-2/3 md:space-x-2 md:mt-0">
               <h5 className="text-xl leading-tight font-medium md:px-3 md:w-1/4 py-1.5">
@@ -140,7 +237,7 @@ const RestaurantTablesSetting = () => {
               </button>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </>
   );
