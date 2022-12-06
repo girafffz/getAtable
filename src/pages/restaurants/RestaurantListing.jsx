b8312077dfa3c9c03d5a8be3be2d9d12ce5b161c
Steph Luo <steph.luokl@gmail.com>
2022-12-06 23:34:18 +0800

    get restaurant api completed

--- src/pages/restaurants/RestaurantListing.jsx
+++ src/pages/restaurants/RestaurantListing.jsx
@@ -1,18 +1,111 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 import Search from "../../components/Search";
 import RestaurantCard from "./RestaurantCard";
 
 const RestaurantListing = (props) => {
-  //   const [searchInput, setSearchInput] = useState("");
-  //   const [isRestaurantListingPage, setIsRestaurantListingPage] = useState(true);
-  console.log("restaurant listing props", props);
+  ////////////////////// STATES ////////////////////
+  //////////////////////////////////////////////////
 
-  const getRestaurants = (searchInput) => {
-    console.log(`fetch api`);
-    console.log(searchInput);
+  const [searchResults, setSearchResults] = useState(undefined);
+  const [capacityResults, setCapacityResults] = useState(undefined);
+  const [fullRestaurantsInfo, setFullRestaurantsInfo] = useState(undefined);
+  const [restaurantCards, setRestaurantCards] = useState();
+
+  ////////////////////// useEffect ////////////////////
+  // fetch all data for restaurants based on search input
+  // if no search input, data for first 12 restaurants will be fetched
+  useEffect(() => {
+    getRestaurants(props.searchInput);
+  }, []);
+
+  // combine different sets of data for each restaurant
+  useEffect(() => {
+    if ((searchResults !== undefined) & (capacityResults !== undefined)) {
+      gatherRestaurantsInfo();
+    }
+  }, [searchResults, capacityResults]);
+
+  // render card for each restaurant
+  useEffect(() => {
+    if (fullRestaurantsInfo !== undefined) {
+      renderRestaurantCards();
+    }
+  }, [fullRestaurantsInfo]);
+
+  //////////////////// FUNCTIONS ///////////////////
+  const getRestaurants = async (input) => {
+    let capacityOfEachRestaurant = [];
+
+    try {
+      // getting basic restaurant info based on search results
+      const res = await fetch(`http://127.0.0.1:5000/api/restaurants`, {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({ searchInput: input }),
+      });
+      const restaurantsData = await res.json();
+      setSearchResults(restaurantsData.data.restaurants);
+      console.log(restaurantsData.data.restaurants);
+
+      // getting capacity data for each restaurant based on search results
+      for (let i = 0; i < restaurantsData.total_results; i++) {
+        const res = await fetch(
+          `http://127.0.0.1:5000/api/restaurants/capacity`,
+          {
+            method: "POST",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify({
+              id: `${restaurantsData.data.restaurants[i].id}`,
+            }),
+          }
+        );
+        const restaurantCapacity = await res.json();
+        capacityOfEachRestaurant.push(restaurantCapacity.data);
+        if (restaurantsData.total_results === capacityOfEachRestaurant.length) {
+          setCapacityResults(capacityOfEachRestaurant);
+        }
+
+        console.log(capacityOfEachRestaurant);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  // how to use search params?
+  const gatherRestaurantsInfo = () => {
+    let restaurantsArray = [];
+
+    for (let i = 0; i < searchResults.length; i++) {
+      for (let j = 0; j < capacityResults.length; j++) {
+        if (i === j) {
+          restaurantsArray.push({
+            restaurant_id: searchResults[i].id,
+            restaurant_name: searchResults[i].name,
+            restaurant_location: searchResults[i].location_id,
+            restaurant_maxCapacity: capacityResults[j].max_capacity,
+            restaurant_seatsAvailable: capacityResults[j].seats_available,
+          });
+        }
+      }
+    }
+    setFullRestaurantsInfo(restaurantsArray);
+  };
+
+  const renderRestaurantCards = () => {
+    const cards = fullRestaurantsInfo.map((restaurant) => {
+      return (
+        <RestaurantCard
+          restaurant_id={restaurant.restaurant_id}
+          restaurant_name={restaurant.restaurant_name}
+          restaurant_location={restaurant.restaurant_location}
+          restaurant_seatsAvailable={restaurant.restaurant_seatsAvailable}
+          restaurant_maxCapacity={restaurant.restaurant_maxCapacity}
+        />
+      );
+    });
+    setRestaurantCards(cards);
+  };
 
   return (
     <>
@@ -24,10 +117,7 @@ const RestaurantListing = (props) => {
       <div className="container mx-auto px-24">
         <h4 className="text-3xl mt-28">Your search results:</h4>
         <br></br>
-
-        <div class="flex flex-wrap justify-between">
-          <RestaurantCard />
-        </div>
+        <div className="flex flex-wrap justify-between">{restaurantCards}</div>
       </div>
     </>
   );
